36247be64d0774550624b607889edf69629a781f
Ivan-PC\Ivan <ivan.vitz.matijas@gmail.com>
2017-10-08 21:22:29 +0200

    Initial Upload
    Game Manager script and Square script

---
+++ GameManager.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour 
+{
+
+	public static GameManager gm;
+    public GameObject XPiece;
+    public GameObject OPiece;
+    private int currPlayer;
+    public GUIText prompt;
+    public Square [] aSquares;
+    public Square[,] aGrid;
+    private bool gameOver = false;
+    private int moves = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (gm == null) 
+		{
+			gm = gameObject.GetComponent<GameManager>();
+		}
+        currPlayer = 1;
+        ShowPlayerPrompt();
+
+        aSquares = FindObjectsOfType(typeof(Square)) as Square[];
+
+        aGrid = new Square[3, 3];
+
+        Square theSquare;
+
+        for (int i = 0; i < aSquares.Length; i++)
+        {
+            theSquare = aSquares[i];
+            aGrid[theSquare.r, theSquare.c] = theSquare;
+        }
+	}
+
+    void ShowPlayerPrompt()
+    {
+        if (currPlayer == 1)
+        {  
+            prompt.text = "Player 1, place an X";
+        }
+        else
+        {
+            prompt.text = "Player 2, place an O";
+        }
+    }
+
+    public void ClickSquare(Square other)
+	{
+        if (gameOver)
+        {  
+            return;
+        }
+        if (currPlayer == 1)
+        {
+            PlacePiece(XPiece, other);
+        }
+        else
+        {
+            PlacePiece(OPiece, other);
+        }
+	}
+	
+    void PlacePiece(GameObject piece, Square other)
+    {
+        moves++;
+        Instantiate(piece, other.gameObject.transform.position, Quaternion.identity);
+        other.player = currPlayer;
+
+        if (CheckForWin(other))
+        {
+            gameOver = true;
+            StartCoroutine(ShowWinnerPrompt());
+            return;
+        }
+        else if (moves >= 9)
+        {
+            gameOver = true;
+            StartCoroutine(ShowTiePrompt());
+            return;
+        }
+        currPlayer++;
+        if (currPlayer > 2)
+        {
+            currPlayer = 1;
+        }
+        ShowPlayerPrompt();
+    }
+
+    int GetPlayer(int r, int c)
+    {
+        return aGrid[r, c].player;
+    }
+
+    bool CheckForWin(Square other)
+    {
+        if ((GetPlayer(other.r, 0) == currPlayer) && (GetPlayer(other.r, 1) == currPlayer) && (GetPlayer(other.r, 2) == currPlayer))
+        {
+            return true;
+        }
+        if ((GetPlayer(0, other.c) == currPlayer) && (GetPlayer(1, other.c) == currPlayer) && (GetPlayer(2, other.c) == currPlayer))
+        {
+            return true;
+        }
+        if ((GetPlayer(0, 0) == currPlayer) && (GetPlayer(1, 1) == currPlayer) && (GetPlayer(2, 2) == currPlayer))
+        {
+            return true;
+        }
+        if ((GetPlayer(0, 2) == currPlayer) && (GetPlayer(1, 1) == currPlayer) && (GetPlayer(2, 0) == currPlayer))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    IEnumerator ShowWinnerPrompt()
+    {
+        if (currPlayer == 1)
+        {
+            prompt.text = "X gets 3 in a row. Player 1 wins!";
+        }
+        else
+        {
+            prompt.text = "O gets 3 in a row. Player 2 wins!";
+        }
+
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator ShowTiePrompt()
+    {
+        prompt.text = "Tie! Neither player wins.";
+
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(0);
+    }
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Square.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour 
+{
+	public int r;
+	public int c;
+    public int player;
+
+	void OnMouseDown()
+	{
+        if (player == 0)
+        {
+            GameManager.gm.ClickSquare(this);
+        }
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
